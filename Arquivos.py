35f4c6a4c9ff048466339702efe468255bc4af99
lucas <lucasucb2020@gmail.com>
2022-01-18 14:00:12 -0300

    Arquivos em Python

---
+++ Arquivos.py
@@ -0,0 +1,22 @@
+"""
+w - cria um arquivo. se ele já existir, sobrescreve
+a - cria um arquivo. se ele já existir, adiciona no fim
+file.read - retorna todo o conteúdo lido
+file.readlines -> retorna uma lista separada por quebras de linha
+
+
+ex1.
+
+
+texto = "Perdi foi tudo kek  xd\n"
+arquivo.write(texto)
+
+"""
+
+arquivo = open('C:\\Users\\delta\\Desktop\\teste.txt', 'r')
+
+x = arquivo.read
+
+print(x)
+
+arquivo.close
